5615b4c4d3cc6aacff24d341fb885fdb8983a0e7
“Edwin <wongyikfei@gmail.com>
2014-04-19 17:50:55 -0400

    edit stock.cs

--- Stock Scouter/Models/Stock.cs
+++ Stock Scouter/Models/Stock.cs
@@ -28,29 +28,88 @@ namespace Stock_Scouter.Models
         }
 
         private double _bidPrice = 0.0;
+        public double BidPrice
+        {
+            get { return this._bidPrice; }
+            set { this._bidPrice = value; }
+        }
 
         private double _lastTradePrice = 0.0;
+        public double LastTradePrice
+        {
+            get { return this._lastTradePrice; }
+            set { this._lastTradePrice = value; }
+        }
 
         private string _lastTradeDate;
+        public string LastTradeDate
+        {
+            get { return this._lastTradeDate; }
+            set { this._lastTradeDate = value; }
+        }
 
         private double _openPrice = 0.0;
+        public double OpenPrice
+        {
+            get { return this._openPrice; }
+            set { this._openPrice = value; }
+        }
 
         private double _dayHighPrice = 0.0;
+        public double DayHighPrice
+        {
+            get { return this._dayHighPrice; }
+            set { this._dayHighPrice = value; }
+        }
 
         private double _dayLowPrice = 0.0;
+        public double DayLowPrice
+        {
+            get { return this._dayLowPrice; }
+            set { this._dayLowPrice = value; }
+        }
 
         private int _volume = 0;
+        public int Volume
+        {
+            get { return this._volume; }
+            set { this._volume = value; }
+        }
 
         private string _weekRange;
+        public string WeekRange
+        {
+            get { return this._weekRange; }
+            set { this._weekRange = value; }
+        }
 
         private string _dayRange;
+        public string DayRange
+        {
+            get { return this._dayRange; }
+            set { this._dayRange = value; }
+        }
 
         private string _dividend;
+        public string Dividend
+        {
+            get { return this._dividend; }
+            set { this._dividend = value; }
+        }
 
         private string _dividendYield;
+        public string DividendYield
+        {
+            get { return this._dividendYield; }
+            set { this._dividendYield = value; }
+        }
 
         private string _peRatio;
-
+        public string PeRatio
+        {
+            get { return this._peRatio; }
+            set { this._peRatio = value; }
+        }
 
         public Stock() {
         }
